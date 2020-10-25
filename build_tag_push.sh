714655d1d4ced83f95b9f6e7a1b08ee31c95f574
Peter Siemen <peter.siemen@googlemail.com>
2020-10-25 17:25:39 +0100

    align tag with collector release

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-IMAGE_TAG=latest
+IMAGE_TAG=1.0.1
 REPO=petersiemen
 NAME=snowplow-stream-collector
 
-docker build -t ${NAME} .
-docker tag ${NAME} ${REPO}/${NAME}:${IMAGE_TAG}
+docker build -t ${NAME}:${IMAGE_TAG} .
+docker tag ${NAME}:${IMAGE_TAG} ${REPO}/${NAME}:${IMAGE_TAG}
 docker push ${REPO}/${NAME}:${IMAGE_TAG}
